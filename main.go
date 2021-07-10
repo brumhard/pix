c6b304a92c6c16ef30e037f1ea3ed062fede5d13
brumhard <fobytobi@googlemail.com>
2021-07-10 20:12:58 +0200

    Add image dir path as argument

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"flag"
 	"io/fs"
 	"log"
 	"net/http"
 	"ogframe/frontend"
 	ownhttp "ogframe/pkg/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/pkg/errors"
 
 	"ogframe/pkg/socket"
 )
@@ -19,8 +22,20 @@ func main() {
 }
 
 func run() error {
+	imgPath := flag.String("images", "", "path to images to be shown on the frame")
+	flag.Parse()
+
+	if imgPath == nil || *imgPath == "" {
+		return errors.New("images argument needs to be set")
+	}
+
+	_, err := os.Stat(*imgPath)
+	if err != nil {
+		return errors.Wrap(err, "images argument should point to a valid path")
+	}
+
 	router := mux.NewRouter()
-	server, err := socket.NewServer("./images")
+	server, err := socket.NewServer(*imgPath)
 	if err != nil {
 		return err
 	}
@@ -32,6 +47,8 @@ func run() error {
 
 	router.Handle("/api/socket", server)
 	router.PathPrefix("/").Handler(ownhttp.NewSPAHandler(http.FS(dist), "index.html"))
+
+	log.Print("server starting")
 
 	return http.ListenAndServe(":8080", router)
 }
