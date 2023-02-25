d2a6c87913cf1cde5ff58c63cd0079a45e2351d4
Tobias Brumhard <code@brumhard.com>
2023-02-25 16:39:36 +0100

    Remove svelte frontend

--- frontend/src/env.d.ts
+++
@@ -1,3 +0,0 @@
-interface ImportMetaEnv {
-    VITE_WS_PATH: string // path to ws endpoint, e.g. /api/socket
-}
--- frontend/src/store/config.ts
+++
@@ -1,7 +0,0 @@
-import {writable, Writable} from "svelte/store";
-
-interface Config {
-    delay: number
-}
-
-export let config: Writable<Config> = writable({delay: 5})
