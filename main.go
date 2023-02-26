cbf66abb36d6d44b83fd6c54cf6804f82cdda506
Tobias Brumhard <code@brumhard.com>
2023-02-26 18:48:34 +0100

    Fix main checks

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/pkg/errors"
 
@@ -21,28 +20,21 @@ func main() {
 }
 
 func run() error {
-	imgPath := flag.String("images", "", "path to images to be shown on the frame")
+	var imgPath string
+	flag.StringVar(&imgPath, "images", "", "path to images to be shown on the frame")
 	flag.Parse()
 
-	if imgPath == nil || *imgPath == "" {
+	if imgPath == "" {
 		return errors.New("images argument needs to be set")
 	}
 
-	_, err := os.Stat(*imgPath)
-	if err != nil {
-		return errors.Wrap(err, "images argument should point to a valid path")
-	}
-
 	router := http.NewServeMux()
-
-	server := socket.NewServer(*imgPath)
-
 	dist, err := fs.Sub(frontend.Static, "build/web")
 	if err != nil {
 		return err
 	}
 
-	router.Handle("/api/socket", server)
+	router.Handle("/api/socket", socket.NewServer(imgPath))
 	router.Handle("/", ownhttp.NewSPAHandler(http.FS(dist), "index.html"))
 
 	log.Print("server starting")
